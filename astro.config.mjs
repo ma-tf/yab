2918dacdd3a3f55db6e4f59f52dfa4cba061ee2c
Matt Fehrenbach <github.4gsly@passmail.com>
2026-06-14 20:45:35 +0200

    feat: add BASE_PATH configuration for dynamic site base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import { BASE_PATH } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
+  base: BASE_PATH,
   site: "https://example.com",
   integrations: [preact()]
 });
