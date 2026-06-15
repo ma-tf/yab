d3bf51a7fb2dc3568e16943cc998904dd8b9481d
Matt Fehrenbach <github.4gsly@passmail.com>
2026-06-15 19:10:08 +0200

    feat: add blog posts and configure content collections for Astro

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,19 @@
 import type { APIContext } from "astro";
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: APIContext) {
+  const posts = await getCollection("blog");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site!,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-gb</language>`,
   });
 }
